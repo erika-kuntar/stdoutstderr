23ffd446bd3c72b6f6386f7fdd3c3f180752ca91
erika-kuntar <50891145+erika-kuntar@users.noreply.github.com>
2024-10-23 17:26:48 -0700

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -5,4 +5,6 @@ def test_stderr_output():
     print("This is a stdrr error message", file=sys.stderr)
     assert False
 
-###
+def test_stdout_output():
+    print("This is a stdrr error message", file=sys.stdout)
+    assert False
